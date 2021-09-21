8a2117c875fcd30b150092ce5e68eefc86edee99
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-09-21 19:59:19 -0400

    react application

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+const db = require('./config/connection');
+const path = require('path');
+const {authMiddleware} = require('./utils/auth');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware 
+});
+serverStrt()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  server.applyMiddleware({ app });
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
+async function serverStrt (){
+await server.start()
+}
