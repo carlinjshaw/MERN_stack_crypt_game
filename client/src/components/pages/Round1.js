2cdd3c2f313a4795635765b9400f474bfbda0009
Eric-JG <guerrero.eric.j@gmail.com>
2021-09-28 15:39:43 -0400

    style the form

--- client/src/components/pages/Round1.js
+++ client/src/components/pages/Round1.js
@@ -1,37 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
-import firstImg from '../../assets/first_img.PNG'
+import { Link } from "react-router-dom";
+import firstImg from "../../assets/first_img.PNG";
 
-
-const round1 = (props) => {
-    return (
-
-      
+const round1 = props => {
+  return (
+    <div class="round1Image" style={{ backgroundImage: `url(${firstImg})` }}>
       <div class="round1">
-        <div>
-          <img class="round1Image" src={firstImg} alt="Snow"></img>
+        <form action="/action_page.php" class="round1form">
+          <h2>Further you walk, and find a fork in the path...</h2>
 
-          <form action="/action_page.php" class="round1form">
-            <h2>Further you walk, and find a fork in the path...</h2>
+          <label>
+            <b>To the left, you hear growlings and the stench is fowl. </b>
+          </label>
+          <button type="submit" class="round1btns">
+            {" "}
+            <Link to="/Monster1" class="txt">
+              {" "}
+              Go Left
+            </Link>
+          </button>
 
-            <label>
-              <b>To the left, you hear growlings and the stench is fowl. </b>
-            </label>
-            <button  type="submit" class="round1btns">  <Link to='/Monster1'>   Go Left</Link>
-             
-            </button>
-
-            <label for="psw">
-              <b>To the right, you see the glimmer of light and warmth.</b>
-            </label>
-            <button type="submit" class="round1btns">
-            <Link to='/event1'>   Go Right</Link>
-            </button>
-          </form>
-        </div>
+          <label for="psw">
+            <b>To the right, you see the glimmer of light and warmth.</b>
+          </label>
+          <button type="submit" class="round1btns">
+            <Link to="/event1" class="txt">
+              {" "}
+              Go Right
+            </Link>
+          </button>
+        </form>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default round1;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-// const { typeDefs, resolvers } = require('./schema');
+const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
 const path = require('path');
 const {authMiddleware} = require('./utils/auth');
@@ -19,13 +19,13 @@ serverStrt()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   server.applyMiddleware({ app });
