69660e18e6dae848ff9a01bfd14c71c746201c56
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-09-29 20:51:02 -0400

    m

--- server/server.js
+++ server/server.js
@@ -17,23 +17,23 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    serverStrt();
   });
 });
 console.log("server")
 async function serverStrt() {
-  await server.start();
   server.applyMiddleware({ app });
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
+  await server.start();
 }
-serverStrt();
